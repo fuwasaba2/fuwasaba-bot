40b0650be68e7d1ed934919437c4fb8d3b44a7c9
Quantum8060 <akaunnt8060@gmail.com>
2024-09-10 01:32:22 +0900

    fix

--- cogs/ban.py
+++ cogs/ban.py
@@ -40,33 +40,5 @@ class ban(commands.Cog):
 
 
 
-class u_ban():
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    @discord.user_command(name="ban", description="指定したユーザーをサーバーからBANします。")
-    @commands.has_any_role(1282384396791320664, 1282384396791320665)
-    async def u_ban(self, ctx, member:discord.Member):
-
-        if member.id == ctx.author.id:
-            await ctx.respond("自分自身をBANすることはできません。", ephemeral=True)
-        elif member.guild_permissions.administrator:
-            await ctx.respond("このコマンドは運営のみ実行できます。", ephemeral=True)
-        else:
-            await ctx.respond(f"{member.mention} がサーバーからBANされました。", ephemeral=True)
-            await member.ban()
-
-    @u_ban.error
-    async def banerror(ctx, error):
-        if isinstance(error, MissingAnyRole):
-            await ctx.respond("あなたはこのコマンドを使用する権限を持っていません!", ephemeral=True)
-        else:
-            await ctx.respond("Something went wrong...", ephemeral=True)
-            raise error
-
-
-
 def setup(bot):
     bot.add_cog(ban(bot))
-    bot.add_cog(u_ban(bot))
