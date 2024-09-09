785ab619093b4eb1f07aa8fc28d761d3566f02fb
Quantum8060 <akaunnt8060@gmail.com>
2024-09-10 01:30:34 +0900

    announceコマンドの修正

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ config_ini = configparser.ConfigParser()
 config_ini.read("config.ini", encoding="utf-8")
 TOKEN = config_ini["MAIN"]["TOKEN"]
 
-owner_role = int(config_ini["ROLE"]["OWNER"])
-admin_role = int(config_ini["ROLE"]["ADMIN"])
-dev_role = int(config_ini["ROLE"]["DEVELOPER"])
+owner_role = config_ini["ROLE"]["OWNER"]
+admin_role = config_ini["ROLE"]["ADMIN"]
+dev_role = config_ini["ROLE"]["DEVELOPER"]
 
 bot = discord.Bot(intents=intents)
 bot.webhooks = {}
-GUILD_IDS = [1235247721934360577]
+GUILD_IDS = [1282384396757893172]
 
 global result
 result = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
@@ -57,9 +57,24 @@ def save_blacklist_data(data):
 
 
 user_dict = {}
+owner_dict = {}
 
-@bot.slash_command(name="announce", description="メッセージを埋め込みにして送信します。")
-@commands.has_any_role(1282384396791320665, 1282384396791320666, 1282384396791320664)
+@bot.slash_command(name="owner", description="メッセージを埋め込みにして送信します。", guild_ids=GUILD_IDS)
+@commands.has_any_role(1282384396791320666)
+async def ana_t(ctx):
+    user_id = ctx.author.id
+
+    if user_id in owner_dict:
+        owner_dict.pop(user_id)
+        await ctx.respond(f"{ctx.author.mention}\nアナウンスモードが終了しました。", ephemeral=True)
+    else:
+        owner_dict[user_id] = ctx.author.name
+        await ctx.respond(f"{ctx.author.mention}\nアナウンスモードを起動しました", ephemeral=True)
+
+
+
+@bot.slash_command(name="admin", description="メッセージを埋め込みにして送信します。", guild_ids=GUILD_IDS)
+@commands.has_any_role(1282384396791320665, 1282384396791320664)
 async def ana_t(ctx):
     user_id = ctx.author.id
 
@@ -70,90 +85,87 @@ async def ana_t(ctx):
         user_dict[user_id] = ctx.author.name
         await ctx.respond(f"{ctx.author.mention}\nアナウンスモードを起動しました", ephemeral=True)
 
+
+
 @bot.event
 async def on_message(message: discord.Message):
     user_id = message.author.id
-    user = message.author
 
-    if user_id in user_dict and not message.author.bot:
-        if owner_role in user.roles:
+    if user_id in owner_dict and not message.author.bot:
 
-            embed = discord.Embed(description=message.content, color=0xe91e62)
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
-            embed.set_footer(text=message.author.id)
+        embed = discord.Embed(description=message.content, color=0xe91e62)
+        embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
 
-            if message.attachments:
-                for attachment in message.attachments:
-                    if attachment.content_type.startswith("image/"):
-                        embed.set_image(url=attachment.url)
-                        break
+        if message.attachments:
+            for attachment in message.attachments:
+                if attachment.content_type.startswith("image/"):
+                    embed.set_image(url=attachment.url)
+                    break
 
-            mentions = [mention.mention for mention in message.mentions]
-            role_mentions = [role.mention for role in message.role_mentions]
-            if message.mention_everyone:
-                mentions.append("@everyone")
-            mention_text = " ".join(mentions + role_mentions)
+        mentions = [mention.mention for mention in message.mentions]
+        role_mentions = [role.mention for role in message.role_mentions]
+        if message.mention_everyone:
+            mentions.append("@everyone")
+        mention_text = " ".join(mentions + role_mentions)
 
-            if message.reference:
-                replied_message = await message.channel.fetch_message(message.reference.message_id)
-
-                if mention_text:
-                    await replied_message.reply(content=mention_text, embed=embed)
-                    await message.delete()
-                    user_dict.pop(user_id)
-                else:
-                    await replied_message.reply(embed=embed)
-                    await message.delete()
-                    user_dict.pop(user_id)
-
+        if message.reference:
+            replied_message = await message.channel.fetch_message(message.reference.message_id)
+            if mention_text:
+                await replied_message.reply(content=mention_text, embed=embed)
+                await message.delete()
+                owner_dict.pop(user_id)
             else:
-                if mention_text:
-                    await message.channel.send(content=mention_text, embed=embed)
-                    await message.delete()
-                    user_dict.pop(user_id)
-                else:
-                    await message.channel.send(embed=embed)
-                    await message.delete()
-                    user_dict.pop(user_id)
+                await replied_message.reply(embed=embed)
+                await message.delete()
+                owner_dict.pop(user_id)
+
         else:
+            if mention_text:
+                await message.channel.send(content=mention_text, embed=embed)
+                await message.delete()
+                owner_dict.pop(user_id)
+            else:
+                await message.channel.send(embed=embed)
+                await message.delete()
+                owner_dict.pop(user_id)
+    elif user_id in user_dict and not message.author.bot:
 
-                embed = discord.Embed(description=message.content, color=0x9b59b6)
-                embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
-                embed.set_footer(text=message.author.id)
+        embed = discord.Embed(description=message.content, color=0x9b59b6)
+        embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
 
-                if message.attachments:
-                    for attachment in message.attachments:
-                        if attachment.content_type.startswith("image/"):
-                            embed.set_image(url=attachment.url)
-                            break
+        if message.attachments:
+            for attachment in message.attachments:
+                if attachment.content_type.startswith("image/"):
+                    embed.set_image(url=attachment.url)
+                    break
 
-                mentions = [mention.mention for mention in message.mentions]
-                role_mentions = [role.mention for role in message.role_mentions]
-                if message.mention_everyone:
-                    mentions.append("@everyone")
-                mention_text = " ".join(mentions + role_mentions)
+        mentions = [mention.mention for mention in message.mentions]
+        role_mentions = [role.mention for role in message.role_mentions]
+        if message.mention_everyone:
+            mentions.append("@everyone")
+        mention_text = " ".join(mentions + role_mentions)
 
-                if message.reference:
-                    replied_message = await message.channel.fetch_message(message.reference.message_id)
+        if message.reference:
+            replied_message = await message.channel.fetch_message(message.reference.message_id)
 
-                    if mention_text:
-                        await replied_message.reply(content=mention_text, embed=embed)
-                        await message.delete()
-                        user_dict.pop(user_id)
-                    else:
-                        await replied_message.reply(embed=embed)
-                        await message.delete()
-                        user_dict.pop(user_id)
+            if mention_text:
+                await replied_message.reply(content=mention_text, embed=embed)
+                await message.delete()
+                user_dict.pop(user_id)
+            else:
+                await replied_message.reply(embed=embed)
+                await message.delete()
+                user_dict.pop(user_id)
 
-                else:
-                    if mention_text:
-                        await message.channel.send(content=mention_text, embed=embed)
-                        await message.delete()
-                        user_dict.pop(user_id)
-                    else:
-                        await message.channel.send(embed=embed)
-                        await message.delete()
-                        user_dict.pop(user_id)
+        else:
+            if mention_text:
+                await message.channel.send(content=mention_text, embed=embed)
+                await message.delete()
+                user_dict.pop(user_id)
+            else:
+                await message.channel.send(embed=embed)
+                await message.delete()
+                user_dict.pop(user_id)
 
 
 
