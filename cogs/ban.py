712422781f83b9c92bd69ec696e77e76cd4c0192
Quantum8060 <akaunnt8060@gmail.com>
2024-09-09 23:51:01 +0900

    ユーザーコマンド追加

--- cogs/ban.py
+++ cogs/ban.py
@@ -1,20 +1,15 @@
 import discord
 from discord.ext import commands
 from discord import Option
-from discord.ext.commands import MissingPermissions
+from discord.ext.commands import MissingPermissions, MissingAnyRole
 import json
+import configparser
 
-Debug_guild = [1235247721934360577]
+config_ini = configparser.ConfigParser()
+config_ini.read("config.ini", encoding="utf-8")
+GUILD = config_ini["MAIN"]["GUILD"]
 
-blacklist_file = 'blacklist.json'
 
-def load_data():
-    with open(blacklist_file, 'r') as file:
-        return json.load(file)
-
-def save_data(data):
-    with open(blacklist_file, 'w') as file:
-        json.dump(data, file, indent=4)
 
 class ban(commands.Cog):
 
@@ -22,32 +17,56 @@ class ban(commands.Cog):
         self.bot = bot
 
     @discord.slash_command(name="ban", description="指定したユーザーをサーバーからBANします。")
-    @commands.has_permissions(administrator = True)
+    @commands.has_any_role(1282384396791320664, 1282384396791320665)
     async def ban(self, ctx, member: Option(discord.Member, description = "BANするユーザーを選択"), reason: Option(str, description = "BAN理由を入力(ログに記載されます。)", required = False)):
-        user_id = str(ctx.author.id)
 
-        data = load_data()
-
-        if user_id not in data:
-            if member.id == ctx.author.id:
-                await ctx.respond("自分自身をBANすることはできません。", ephemeral=True)
-            elif member.guild_permissions.administrator:
-                await ctx.respond("このコマンドは管理者のみ実行できます。", ephemeral=True)
-            else:
-                if reason == None:
-                    reason = f"BAN理由:{ctx.author}"
-                await member.ban(reason = reason)
-                await ctx.respond(f"<@{member.id}> がサーバーからBANされました。\n\nBAN理由: {reason}", ephemeral=True)
+        if member.id == ctx.author.id:
+            await ctx.respond("自分自身をBANすることはできません。", ephemeral=True)
+        elif member.guild_permissions.administrator:
+            await ctx.respond("このコマンドは管理者のみ実行できます。", ephemeral=True)
         else:
-            await ctx.response.send_message("あなたはブラックリストに登録されています。", ephemeral=True)
+            if reason == None:
+                reason = f"BAN理由:{ctx.author}"
+            await member.ban(reason = reason)
+            await ctx.respond(f"<@{member.id}> がサーバーからBANされました。\n\nBAN理由: {reason}", ephemeral=True)
 
     @ban.error
     async def banerror(ctx, error):
-        if isinstance(error, MissingPermissions):
+        if isinstance(error, MissingAnyRole):
             await ctx.respond("あなたはこのコマンドを使用する権限を持っていません!", ephemeral=True)
         else:
             await ctx.respond("Something went wrong...", ephemeral=True)
             raise error
 
+
+
+class u_ban():
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.user_command(name="ban", description="指定したユーザーをサーバーからBANします。")
+    @commands.has_any_role(1282384396791320664, 1282384396791320665)
+    async def u_ban(self, ctx, member:discord.Member):
+
+        if member.id == ctx.author.id:
+            await ctx.respond("自分自身をBANすることはできません。", ephemeral=True)
+        elif member.guild_permissions.administrator:
+            await ctx.respond("このコマンドは運営のみ実行できます。", ephemeral=True)
+        else:
+            await ctx.respond(f"{member.mention} がサーバーからBANされました。", ephemeral=True)
+            await member.ban()
+
+    @u_ban.error
+    async def banerror(ctx, error):
+        if isinstance(error, MissingAnyRole):
+            await ctx.respond("あなたはこのコマンドを使用する権限を持っていません!", ephemeral=True)
+        else:
+            await ctx.respond("Something went wrong...", ephemeral=True)
+            raise error
+
+
+
 def setup(bot):
     bot.add_cog(ban(bot))
+    bot.add_cog(u_ban(bot))
--- cogs/kick.py
+++ cogs/kick.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import Option
-from discord.ext.commands import MissingPermissions
+from discord.ext.commands import MissingAnyRole
 import json
 
 Debug_guild = [1235247721934360577]
@@ -22,32 +22,55 @@ class kick(commands.Cog):
         self.bot = bot
 
     @discord.slash_command(name="kick", description="指定したユーザーをサーバーからキックします。")
-    @commands.has_permissions(administrator = True)
+    @commands.has_any_role(1282384396791320664, 1282384396791320665)
     async def kick(self, ctx, member: Option(discord.Member, description = "キックするユーザーを選択"), reason: Option(str, description = "キック理由を入力(ログに記載されます。)", required = False)):
-        user_id = str(ctx.author.id)
 
-        data = load_data()
-
-        if user_id not in data:
-            if member.id == ctx.author.id:
-                await ctx.respond("自分自身をkickすることはできません。", ephemeral=True)
-            elif member.guild_permissions.administrator:
-                await ctx.respond("このコマンドは管理者のみ実行できます。", ephemeral=True)
-            else:
-                if reason == None:
-                    reason = f"kick理由:{ctx.author}"
-                await member.kick(reason = reason)
-                await ctx.respond(f"<@{member.id}> がサーバーからキックされました。\n\nキック理由: {reason}", ephemeral=True)
+        if member.id == ctx.author.id:
+            await ctx.respond("自分自身をkickすることはできません。", ephemeral=True)
+        elif member.guild_permissions.administrator:
+            await ctx.respond("このコマンドは運営のみ実行できます。", ephemeral=True)
         else:
-            await ctx.response.send_message("あなたはブラックリストに登録されています。", ephemeral=True)
+            if reason == None:
+                reason = f"kick理由:{ctx.author}"
+            await ctx.respond(f"{member.mention}がサーバーからキックされました。\n\nキック理由: {reason}", ephemeral=True)
+            await member.kick(reason = reason)
 
     @kick.error
     async def banerror(ctx, error):
-        if isinstance(error, MissingPermissions):
+        if isinstance(error, MissingAnyRole):
             await ctx.respond("あなたはこのコマンドを使用する権限を持っていません!", ephemeral=True)
         else:
             await ctx.respond("Something went wrong...", ephemeral=True)
             raise error
 
+
+
+class u_kick():
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.user_command(name="kick", description="指定したユーザーをサーバーからキックします。")
+    @commands.has_any_role(1282384396791320664, 1282384396791320665)
+    async def u_kick(self, ctx, member:discord.Member):
+
+        if member.id == ctx.author.id:
+            await ctx.respond("自分自身をBANすることはできません。", ephemeral=True)
+        elif member.guild_permissions.administrator:
+            await ctx.respond("このコマンドは運営のみ実行できます。", ephemeral=True)
+        else:
+            await ctx.respond(f"{member.mention} がサーバーからキックされました。", ephemeral=True)
+            await member.kick()
+
+    @u_kick.error
+    async def banerror(ctx, error):
+        if isinstance(error, MissingAnyRole):
+            await ctx.respond("あなたはこのコマンドを使用する権限を持っていません!", ephemeral=True)
+        else:
+            await ctx.respond("Something went wrong...", ephemeral=True)
+            raise error
+
+
 def setup(bot):
     bot.add_cog(kick(bot))
+    bot.add_cog(u_kick(bot))
