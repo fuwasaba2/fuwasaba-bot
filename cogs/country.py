8f9d8e40106856918f8beb7b9bf4fa9078804c3e
Quantum8060 <akaunnt8060@gmail.com>
2024-09-10 23:58:29 +0900

    国家への所属機能を実装

--- cogs/country.py
+++ cogs/country.py
@@ -112,6 +112,39 @@ class permissionView(discord.ui.View):
 
 
 
+class joinView(discord.ui.View):
+
+    def __init__(self, bot):
+        super().__init__(timeout=None)
+        self.bot = bot
+
+    @discord.ui.button(label="承認", custom_id="join-button", style=discord.ButtonStyle.green)
+    async def permission(self, button: discord.ui.Button, interaction: discord.Interaction):
+
+        country_id = str(join_c)
+        employee_id = str(join_u.id)
+
+        await add_employee(country_id, employee_id)
+
+        await interaction.response.send_message(f"{join_u.display_name}の国家への加入を承認しました。")
+
+        embed = discord.Embed(title="国家加入", description="以下の内容で国家への加入が行われました。", color=0x4169e1)
+        embed.add_field(name="加入者", value=join_u.mention, inline=False)
+        embed.add_field(name="加入先", value=join_c, inline=False)
+
+        approval_c = await self.bot.fetch_channel("1282384397563068506")
+        await approval_c.send(embed=embed)
+
+    @discord.ui.button(label="却下", custom_id="kick-button", style=discord.ButtonStyle.red)
+    async def rejection(self, button: discord.ui.Button, interaction: discord.Interaction):
+
+        await interaction.response.send_message("加入を却下しました。")
+
+        applicant_dm = await self.bot.fetch_user(f"{join_u.id}")
+        await applicant_dm.send("加入が却下されました。")
+
+
+
 class country(commands.Cog):
 
     def __init__(self, bot):
@@ -167,11 +200,23 @@ class country(commands.Cog):
     @country.command(name="request", description="指定した国家に加入申請を行います。", guild_ids=GUILD_IDS)
     async def request(self, ctx: discord.ApplicationContext, name: discord.Option(str, description="加入したい国家の名称を入力してください。")):
 
+        global join_c, join_u
+        join_c = name
+        join_u = ctx.author
+
+        ruler_id = get_user_info(name)
+
         embed = discord.Embed(title="国家加入申請", description="以下の内容で入国申請が届きました。")
         embed.add_field(name="入国希望者", value=ctx.author.mention, inline=False)
+        embed.add_field(name="国主", value=f"<@!{ruler_id[1]}>")
         embed.set_thumbnail(url=ctx.author.display_avatar.url)
 
-        await ctx.respond(embed=embed)
+        View = joinView(self.bot)
+        ruler_dm = await self.bot.fetch_user(ruler_id[1])
+        await ruler_dm.send(embed=embed, view=View)
+
+        await ctx.respond(f"{name}への加入申請を行いました。\n国主が承認するまでお待ちください。\n \nなお、承認及び不承認の際にはBOTからのDMが届きます。\n必ずBOTからメッセージを受信できるようにしてください。")
+
 
 
 def setup(bot):
